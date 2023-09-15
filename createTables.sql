b0ff281e6512a43f17bbac17af17c1c2cf363f75
Barbara Dreyer Rentz Ribeiro <53235549+bribeiro33@users.noreply.github.com>
2023-09-15 14:04:07 -0400

    createTables ready for testing

--- createTables.sql
+++ createTables.sql
@@ -5,18 +5,19 @@ CREATE TABLE Users (
     year_of_birth INTEGER,
     month_of_birth INTEGER,
     day_of_birth INTEGER,
-    gender VARCHAR2(100),
-)
+    gender VARCHAR2(100)
+);
 
 CREATE TABLE Friends (
     user1_id INTEGER NOT NULL,
     user2_id INTEGER NOT NULL,
     PRIMARY KEY (user1_id, user2_id),
-    FOREIGN KEY (user1_id) REFERENCES Users (user_id),
-    FOREIGN KEY (user2_id) REFERENCES Users (user_id),
-    CHECK (user1_id != user2_id)
-    -- TODO: constraints
-)
+    FOREIGN KEY (user1_id) REFERENCES Users (user_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (user2_id) REFERENCES Users (user_id)
+    ON DELETE CASCADE,
+    CHECK (user1_id <> user2_id)
+);
 
 CREATE TRIGGER Order_Friend_Pairs
     BEFORE INSERT ON Friends
@@ -35,27 +36,27 @@ CREATE TABLE Cities (
     city_id INTEGER PRIMARY KEY, 
     city_name VARCHAR2(100) NOT NULL,
     state_name VARCHAR2(100) NOT NULL,
-    country_name VARCHAR2(100) NOT NULL
-    -- TODO: constraints
-)
+    country_name VARCHAR2(100) NOT NULL,
+    UNIQUE (city_name, state_name, country_name)
+);
 
 CREATE TABLE User_Current_Cities (
-    user_id INTEGER NOT NULL,
-    current_city_id INTEGER NOT NULL, 
-    PRIMARY KEY (user_id, current_city_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (current_city_id) REFERENCES Cities (city_id),
-    -- TODO: constraints
-)
+    user_id INTEGER PRIMARY KEY, -- only user_id as PK bc key constraint
+    current_city_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (user_id) 
+    ON DELETE CASCADE,
+    FOREIGN KEY (current_city_id) REFERENCES Cities (city_id) 
+    ON DELETE CASCADE
+);
 
 CREATE TABLE User_Hometown_Cities (
-    user_id INTEGER NOT NULL,
+    user_id INTEGER PRIMARY KEY,
     hometown_city_id INTEGER NOT NULL, 
-    PRIMARY KEY (user_id, hometown_city_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id),
-    FOREIGN KEY (hometown_city_id) REFERENCES Cities (city_id),
-    -- TODO: constraints
-)
+    FOREIGN KEY (user_id) REFERENCES Users (user_id) 
+    ON DELETE CASCADE,
+    FOREIGN KEY (hometown_city_id) REFERENCES Cities (city_id)
+    ON DELETE CASCADE
+);
 
 CREATE TABLE Messages (
     message_id INTEGER PRIMARY KEY,
@@ -63,28 +64,30 @@ CREATE TABLE Messages (
     receiver_id INTEGER NOT NULL,
     message_content VARCHAR2(2000) NOT NULL,
     sent_time TIMESTAMP NOT NULL,
-    FOREIGN KEY (sender_id) REFERENCES Users (user_id),
-    FOREIGN KEY (receiver_id) REFERENCES Users (user_id)
-    -- TODO: constraints
-)
+    FOREIGN KEY (sender_id) REFERENCES Users (user_id) 
+    ON DELETE CASCADE,
+    FOREIGN KEY (receiver_id) REFERENCES Users (user_id) 
+    ON DELETE CASCADE
+);
 
 CREATE TABLE Programs (
     program_id INTEGER PRIMARY KEY,
     institution VARCHAR2(100) NOT NULL,
     concentration VARCHAR2(100) NOT NULL,
-    degree VARCHAR2(100) NOT NULL
-    -- TODO: constraints
-)
+    degree VARCHAR2(100) NOT NULL,
+    UNIQUE(institution, concentration, degree)
+);
 
 CREATE TABLE Education (
     user_id INTEGER NOT NULL,
     program_id INTEGER NOT NULL, 
     program_year INTEGER NOT NULL,
-    PRIMARY KEY (user_id,program_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id),
+    PRIMARY KEY (user_id, program_id, program_year), -- Could also use UNIQUE?
+    FOREIGN KEY (user_id) REFERENCES Users (user_id)
+    ON DELETE CASCADE,
     FOREIGN KEY (program_id) REFERENCES Programs (program_id)
-    -- TODO: constraints
-)
+    ON DELETE CASCADE
+);
 
 CREATE TABLE User_Events (
     event_id INTEGER PRIMARY KEY,
@@ -99,21 +102,24 @@ CREATE TABLE User_Events (
     event_city_id INTEGER NOT NULL,
     event_start_time TIMESTAMP,
     event_end_time TIMESTAMP,
-    FOREIGN KEY (event_creator_id) REFERENCES Users (user_id),
-    FOREIGN KEY (event_city_id) REFERENCES Cities (city_id)
-    -- TODO: constraints
-)
+    FOREIGN KEY (event_creator_id) REFERENCES Users (user_id) 
+    ON DELETE CASCADE,
+    FOREIGN KEY (event_city_id) REFERENCES Cities (city_id) 
+    ON DELETE CASCADE
+);
 
 CREATE TABLE Participants (
     event_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     confirmation VARCHAR2(100) NOT NULL,
     PRIMARY KEY (event_id, user_id),
-    FOREIGN KEY (event_id) REFERENCES Users_Events (event_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id),
+    FOREIGN KEY (event_id) REFERENCES Users_Events (event_id) 
+    ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES Users (user_id) 
+    ON DELETE CASCADE,
     CHECK (confirmation IN ('Attending', 'Unsure', 'Declines', 'Not_Replied'))
     -- TODO: constraints
-)
+);
 
 CREATE TABLE Albums (
     album_id INTEGER PRIMARY KEY,
@@ -124,10 +130,10 @@ CREATE TABLE Albums (
     album_link VARCHAR2(2000) NOT NULL,
     album_visibility VARCHAR2(100) NOT NULL,
     cover_photo_id INTEGER NOT NULL,
-    FOREIGN KEY (album_owner_id) REFERENCES Users (user_id),
+    FOREIGN KEY (album_owner_id) REFERENCES Users (user_id)
+    ON DELETE CASCADE,
     CHECK (album_visibility IN ('Everyone, Friends, Friends_Of_Friends, Myself'))
-    -- TODO: constraints
-)
+);
 
 CREATE TABLE Photos (
     photo_id INTEGER PRIMARY KEY,
@@ -138,12 +144,11 @@ CREATE TABLE Photos (
     photo_link VARCHAR2(2000) NOT NULL,
     FOREIGN KEY (album_id) REFERENCES Albums (album_id)
     INITIALLY DEFERRED DEFERRABLE
-    -- TODO: constraints
-)
+);
 
 ALTER TABLE ALBUMS
 ADD CONSTRAINT album_to_photos_cxn
-FOREIGN KEY (cover_photo_id) REFERENCES Photos (photo_id) 
+FOREIGN KEY (cover_photo_id) REFERENCES Photos (photo_id) ON DELETE CASCADE
 INITIALLY DEFERRED DEFERRABLE;
 
 CREATE TABLE Tags (
@@ -153,10 +158,11 @@ CREATE TABLE Tags (
     tag_x NUMBER NOT NULL,
     tag_y NUMBER NOT NULL,
     PRIMARY KEY (tag_photo_id, tag_subject_id),
-    FOREIGN KEY (tag_photo_id) REFERENCES Photos (photo_id),
-    FOREIGN KEY (tag_subject_id) REFERENCES Users (user_id)
-    -- TODO: constraints
-)
+    FOREIGN KEY (tag_photo_id) REFERENCES Photos (photo_id) 
+    ON DELETE CASCADE,
+    FOREIGN KEY (tag_subject_id) REFERENCES Users (user_id) 
+    ON DELETE CASCADE
+);
 
 CREATE SEQUENCE Cities_Sequence
     START WITH 1
